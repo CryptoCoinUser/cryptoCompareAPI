23fc01c9727c8feff0b672b5a9025d9f0c73a932
CryptoCoinUser <adkantor@gmail.com>
2017-03-21 15:38:28 -0700

    does totals for pre-populated coins qtys, but not the grand total

--- cryptocompare.js
+++ cryptocompare.js
@@ -1,25 +1,20 @@
 function getDataFromApi(queryObject, callback) {
   var BASE_URL = 'https://min-api.cryptocompare.com/data/price';
-  //?fsym=ETH&tsyms=BTC,USD,EUR
   var query = { fsym: queryObject.fsym,
-                //tsyms: ['ETH','USD', 'EUR', 'LTC'].toString()
-                tsyms: queryObject.tsymsArray.toString()             
+                tsyms: queryObject.tsymsArray.toString() //tsyms: ['ETH','USD', 'EUR', 'LTC'].toString()            
              }
   $.getJSON(BASE_URL, query, callback);
 }
-
-/*
-getDataFromApi('notUsed', function(RES){
-  console.log(RES);
-});
-*/
 
 function getCoinFromRow(row){
 	var coin;
 	coin = $(row).find('td.coin span.apiName').html();
   return coin;
+}
 
-
+function getPriceIn(table){
+  var priceIn = $('table').find('select.priceIn option:selected').val();
+  return priceIn;
 }
 
 function lookupPriceAndDisplayItInRow(coin, priceIn, htmlRow){
@@ -34,26 +29,106 @@ function lookupPriceAndDisplayItInRow(coin, priceIn, htmlRow){
       $(htmlRow).find('td.price').html(price);
     }
     // syncronous vs async: how do I RETURN the price so i can pass it to another function? use Promise?
-  
 }
-//
+
+$(document).ready(function(){
+  //assuming some asset rows
+  refreshPrices();
+  setTimeout(function(){
+    updateTotals();
+  }, 1000); 
+});
 
 $('table').on('change', 'select.priceIn', function(event){
   refreshPrices();
+  updateTotals();
 });
 
 function refreshPrices(){
-  $('table tr.asset').each(function(row){
-    var htmlRow = $('table tr.asset')[row];
+  $('table tr.asset').each(function(rowNumber){
+    var htmlRow = $('table tr.asset')[rowNumber];
     var coin = getCoinFromRow(htmlRow);
     var priceIn = getPriceIn($('table'));
     lookupPriceAndDisplayItInRow(coin, priceIn, htmlRow);
   })
 }
 
-function getPriceIn(table){
-  var priceIn = $('table').find('select.priceIn option:selected').val();
-  console.log('getPriceIn:');
-  console.log(priceIn);
-  return priceIn;
+$('table').on('change', 'input.qty', function(event){
+  //refreshPrices();
+  //var qty = $(this).val();
+  updateTotal($(this).closest('tr.asset'));
+});
+
+function updateTotal(row){
+  var qty = getQty(row);
+  var price = getPrice(row);
+  var total = qty * price;
+  $(row).find('td.total').html(total);
+  // console.log('updateTotal');
+  // console.log(qty);
+  // console.log(price);
+  // console.log(total);
 }
+
+function updateTotals(){
+  console.log('updateTotals() called');
+  var grandTotal = 0;
+  $('table tr.asset').each(function(rowNumber){
+    var htmlRow = $('table tr.asset')[rowNumber];
+    
+    console.log('updateTotals() for each row');
+    
+    console.log('htmlRow');
+    console.log(htmlRow);
+    
+    console.log('calling updateTotal');
+    updateTotal(htmlRow);
+
+    console.log('calling getTotal');
+    var total = getTotal(htmlRow);
+    console.log('total');
+    console.log(total);
+    grandTotal += total;
+
+
+
+  })
+  $('td.gradTotal').html(grandTotal);
+  //console.log(grandTotal);
+}
+
+function getQty(row){
+  var qty = $(row).find('input.qty').val();
+  //console.log('getQty');
+  //console.log(qty);
+  //console.log(row);
+  if(!qty){
+    console.log('getQty: qty is false');
+    console.log('row is');
+    console.log(row);
+    console.log('qty is');
+    console.log(qty);
+  }
+  return qty;
+}
+function getPrice(row){
+  var price = $(row).find('td.price').html()
+  return price;
+}
+function getTotal(row){
+  var total = $(row).find('input.total').val();
+  if(!total){
+    total = updateTotal(row);
+    // console.log('getTotal: total is false');
+    // console.log(row);
+    // console.log('total');
+    // console.log(total);
+  }
+
+  return total;
+}
+
+
+
+
+
